3c55f175c2562f891dfa366e3215f809b0d0b7bb
Yap Hong Kheng <hongkheng.yap@gmail.com>
2014-05-20 22:15:50 +0800

    Add circle element.

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 (function() {
 	
+	var svgContainer = d3.select("#simple").append("svg")
+										   .attr("id", "svgContain")
+										   .attr("width", 300)
+										   .attr("height", 300);
+	// Draw rectangle
+	var rectangle = svgContainer.append("rect").style("fill", "#cccccc")
+											.attr("id", "bg")
+											.attr("width", 300)
+											.attr("height", 300);
 
-	
+
+	// Draw circle
+	function makeCircle() {
+		var circle = svgContainer.append("circle")
+							.attr("cx", 30)
+							.attr("cy", 30)
+							.attr("r", 20);	
+	}
+
+	makeCircle();
+
 })();
